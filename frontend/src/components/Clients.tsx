44234c6a89c22024331c09e6d69608280bd80271
SFS <sfstricks@hotmail.com>
2023-03-04 13:56:20 -0500

    avances

--- frontend/src/components/Clients.tsx
+++ frontend/src/components/Clients.tsx
@@ -19,69 +19,51 @@ export default function Clients() {
   ];
 
   return (
-    <div>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-            >
-              ID
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-            >
-              Name
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
-            >
-              Email
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
-            >
-              Count
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
-            >
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {example_data.map( row => 
-            <tr>
-              <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
-                {row.id}
-              </td>
-              <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                {row.name}
-              </td>
-              <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                {row.email}
-              </td>
-              <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
-                {row.count}
-              </td>
-              <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
-                <a
-                  className="text-green-500 hover:text-green-700"
-                  href="#"
-                >
-                  Increment
-                </a>
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+    <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        ID
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Email
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Count
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+              {example_data.map( row => 
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {row.id}
+                    </th>
+                    <td className="px-6 py-4">
+                      {row.name}  
+                    </td>
+                    <td className="px-6 py-4">
+                      {row.email}
+                    </td>
+                    <td className="px-6 py-4">
+                      {row.count}
+                    </td>
+                    <td className="px-6 py-4">
+                      <button type="button" className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        Alternative
+                      </button>
+                    </td>
+                </tr>
+              )}
+            </tbody>
+        </table>
     </div>
   );
 }
